5cd242d3c23e9d210cc0a0ea9ead339305bcceca
OmonovSarvar <sarvaromon33@gmail.com>
2024-01-05 07:08:02 +0500

    Added CBV for Txt file

--- conventer/urls.py
+++ conventer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from .views import TextView, DocxLatinAPIView, DocxCyrillicAPIView
+from .views import TextView, DocxLatinAPIView, DocxCyrillicAPIView, TxtLatinAPIView, TxtCyrillicAPIView
 
 urlpatterns = [
     path('', TextView.as_view(), name='text'),
     path('docx/latin', DocxLatinAPIView.as_view(), name='docx-latin'),
     path('docx/cyrillic', DocxCyrillicAPIView.as_view(), name='docx-cyrillic'),
+    path('txt/latin', TxtLatinAPIView.as_view(), name='txt-latin'),
+    path('txt/cyrillic', TxtCyrillicAPIView.as_view(), name='txt-cyrillic')
 ]
--- conventer/views.py
+++ conventer/views.py
@@ -1,11 +1,12 @@
-from mediafiles.docx_files.for_docx import docx_convert_to_latin
-from .for_text import text_convert_to_latin, text_convert_to_cyrillic
-from .serializer import DocxSerializer
-
 from rest_framework import status
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from mediafiles.txt_files.for_txt import txt_conventer_to_cyrillic
+from .for_text import text_convert_to_latin, text_convert_to_cyrillic
+from .serializer import DocxSerializer, TxtFileSerializer
+from mediafiles.docx_files.for_docx import docx_convert_to_latin, docx_conventer_to_cyrillic
 
 
 class DocxLatinAPIView(APIView):
@@ -15,8 +16,6 @@ class DocxLatinAPIView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            # you can access the file like this from serializer
-            # uploaded_file = serializer.validated_data["file"]
             serializer.save()
             yolak = serializer.data['docx_file']
             yolak = str(yolak)
@@ -40,13 +39,61 @@ class DocxCyrillicAPIView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            # you can access the file like this from serializer
-            # uploaded_file = serializer.validated_data["file"]
             serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+            yolak = serializer.data['docx_file']
+            yolak = str(yolak)
+            print(yolak)
+            yolak = yolak.replace("/media/docx_files/", "")
+            response = docx_conventer_to_cyrillic(yolak)
+            return Response({
+                "result": response
+            })
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class TxtCyrillicAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = TxtFileSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            yolak = serializer.data['txt']
+            yolak = str(yolak)
+            print(yolak)
+            yolak = yolak.replace("/media/txt_files/", "")
+            response = txt_conventer_to_cyrillic(yolak)
+            return Response({
+                "result": response
+            })
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class TxtLatinAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = TxtFileSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            yolak = serializer.data['txt']
+            yolak = str(yolak)
+            print(yolak)
+            yolak = yolak.replace("/media/txt_files/", "")
+            response = txt_conventer_to_cyrillic(yolak)
+            return Response({
+                "result": response
+            })
 
         return Response(
             serializer.errors,
